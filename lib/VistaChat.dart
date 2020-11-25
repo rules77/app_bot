e6f322a9518f693189768fb43c58a759fbac1f5a
rules77 <raulbermudezrobles@hotmail.com>
2020-11-24 22:02:29 -0600

    initial release

---
+++ lib/VistaChat.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:bubble/bubble.dart';
+import 'package:http/http.dart' as http;
+
+class VistaChat extends StatefulWidget {
+  @override
+  _VistaChatState createState() => _VistaChatState();
+}
+
+class _VistaChatState extends State<VistaChat> {
+  final GlobalKey<AnimatedListState> _listKey = GlobalKey();
+  List<String> _data = [];
+  static const String BOT_URL = "";
+
+  TextEditingController _queryController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Flutter & Python"),
+      ),
+      body: Stack(
+        children: <Widget>[
+          AnimatedList(
+              key: _listKey,
+              initialItemCount: _data.length,
+              itemBuilder:
+                  (BuildContext context, int index, Animation animation) {
+                return _buildItem(_data[index], animation, index);
+              }),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: TextField(
+              decoration: InputDecoration(
+                icon: Icon(
+                  Icons.message,
+                  color: Colors.greenAccent,
+                ),
+                hintText: "Hello",
+              ),
+              controller: _queryController,
+              textInputAction: TextInputAction.send,
+              onSubmitted: (msg) {
+                this._getResponse();
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  http.Client _getClient() {
+    return http.Client();
+  }
+
+  void _getResponse() {
+    if (_queryController.text.length > 0) {
+      this._insertSingleItem(_queryController.text);
+      var client = _getClient();
+      try {
+        client.post(
+          BOT_URL,
+          body: {"query": _queryController.text},
+        )..then((response) {
+            Map<String, dynamic> data = jsonDecode(response.body);
+            _insertSingleItem(data['response'] + "<bot>");
+          });
+      } catch (e) {
+        print("Failed -> $e");
+      } finally {
+        client.close();
+        _queryController.clear();
+      }
+    }
+  }
+
+  void _insertSingleItem(String message) {
+    _data.add(message);
+    _listKey.currentState.insertItem(_data.length - 1);
+  }
+
+  Widget _buildItem(String item, Animation animation, int index) {
+    bool mine = item.endsWith("<bot>");
+    return SizeTransition(
+      sizeFactor: animation,
+      child: Padding(
+        padding: EdgeInsets.only(top: 10),
+        child: Container(
+          alignment: mine ? Alignment.topLeft : Alignment.topRight,
+          child: Bubble(
+            child: Text(item.replaceAll("<bot>", "")),
+            color: mine ? Colors.blue : Colors.indigo,
+            padding: BubbleEdges.all(10),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/griddashboard.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'VistaChat.dart';
+
+class GridDashboard extends StatelessWidget {
+  Items item1 = new Items(
+      title: "Reportes",
+      subtitle: "March, Wednesday",
+      event: "3 Events",
+      img: "assets/settings.png");
+
+  Items item2 = new Items(
+      title: "Opciones de Administrador",
+      subtitle: "March, Wednesday",
+      event: "3 Events",
+      img: "assets/settings.png");
+
+  Items item3 = new Items(
+      title: "Configuración del chat",
+      subtitle: "March, Wednesday",
+      event: "3 Events",
+      img: "assets/settings.png");
+
+  Items item4 = new Items(
+      title: "Historial de conversaciones",
+      subtitle: "March, Wednesday",
+      event: "3 Events",
+      img: "assets/settings.png");
+
+  Items item5 = new Items(
+      title: "Permisos y visualización",
+      subtitle: "March, Wednesday",
+      event: "3 Events",
+      img: "assets/settings.png");
+
+  Items item6 = new Items(
+      title: "Acerca de...",
+      subtitle: "March, Wednesday",
+      event: "3 Events",
+      img: "assets/settings.png");
+
+  @override
+  Widget build(BuildContext context) {
+    List<Items> myList = [item1, item2, item3, item4, item5, item6];
+    var color = 0xff453658;
+    //return Scaffold(
+    //  backgroundColor: Color(0xff392850),
+    //);
+    return Flexible(
+      child: GridView.count(
+        childAspectRatio: 1.0,
+        padding: EdgeInsets.only(
+          left: 16,
+          right: 16,
+        ),
+        crossAxisCount: 2,
+        crossAxisSpacing: 18,
+        mainAxisSpacing: 18,
+        children: myList.map((data) {
+          return Container(
+            decoration: BoxDecoration(
+                color: Color(color), borderRadius: BorderRadius.circular(10)),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                new InkWell(
+                  child: Image.asset(
+                    data.img,
+                    width: 42,
+                  ),
+                  onTap: () {
+                    print("Clicked container");
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => VistaChat()),
+                    );
+                  },
+                ),
+                //Image.asset(
+                //  data.img,
+                //  width: 42,
+                //),
+                SizedBox(
+                  height: 14,
+                ),
+                Text(
+                  data.title,
+                  style: GoogleFonts.openSans(
+                      textStyle: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600)),
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  data.subtitle,
+                  style: GoogleFonts.openSans(
+                      textStyle: TextStyle(
+                          color: Colors.white38,
+                          fontSize: 10,
+                          fontWeight: FontWeight.w600)),
+                ),
+                SizedBox(
+                  height: 14,
+                ),
+                Text(
+                  data.event,
+                  style: GoogleFonts.openSans(
+                      textStyle: TextStyle(
+                          color: Colors.white70,
+                          fontSize: 11,
+                          fontWeight: FontWeight.w600)),
+                ),
+              ],
+            ),
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
+
+class Items {
+  String title;
+  String subtitle;
+  String event;
+  String img;
+  Items({this.title, this.subtitle, this.event, this.img});
+}
